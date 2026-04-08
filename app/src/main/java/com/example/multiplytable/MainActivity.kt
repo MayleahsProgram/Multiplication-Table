cfb4f3df33eb6e0e18a1671183ca738bee7b595c
May-Leah Schwartz <mayleahs26@outlook.com>
2026-04-08 08:52:36 +0200

    new code added

--- app/src/main/java/com/example/multiplytable/MainActivity.kt
+++ app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.multiplytable
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        //Variables for design elements
+        val numInput =findViewById<EditText>(R.id.numInput)
+        val multiplyBtn = findViewById<Button>(R.id.multiplyBtn)
+
+        //Button click listener for multiplication table
+        multiplyBtn.setOnClickListener {
+            val intent = Intent(this, TableDisplay::class.java)
+            intent.putExtra("tableNumber",numInput.text.toString()
+            startActivity(intent))
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/multiplytable/TableDisplay.kt
@@ -0,0 +1,56 @@
+package com.example.multiplytable
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class TableDisplay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_table_display)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        
+        //Getting data from intent
+        val tableString = intent.getStringExtra("tableNumber")
+        
+        //Use toInOrNull to prevent crash if input is not  a number
+        val tableNumber = tableString?.length ?: 0
+        val textView = findViewById<TextView>(R.id.textView)
+        var tableText = "$tableNumber x table\n\n"
+        
+        var counter = 1
+        while (counter <= 10){
+            val answer = tableNumber * counter
+            tableText += "$tableNumber x $counter = $answer\n\n"
+            counter++
+        }
+
+        tableText.also { textView.text = it }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
